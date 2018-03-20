e2087ee347ab78f2ca39d306ab7c0ec1b67a7742
Florfriedli <florfriedli@gmail.com>
2018-03-20 20:32:52 -0300

    polarimetro con T°
    Se agrega el sensado de la temperatura a todo el programa anda

--- Programa polarimetro/Programa_polarimetro_18-3_v2/Programa_polarimetro_18-3_v2.ino
+++ Programa polarimetro/Programa_polarimetro_18-3_v2/Programa_polarimetro_18-3_v2.ino
@@ -7,7 +7,6 @@
 //Cable marron --> GND
 //Cable naranja --> pin 
 
-//#include <TimerOne.h> //Libreria para poder interrumpir por Timer1
 #include <Wire.h>     //Declaración de libreria para usar modulo I2C
 #include <LiquidCrystal_I2C.h>  //Declaracion de libreria para LCD
 #include <Servo.h> //Declaración de libreria para el servomotor
@@ -37,7 +36,7 @@ int pulsador1_anterior = 0; //Variable para verificar el estado de los pulsadore
 int pulsador2_anterior = 0;
 int pulsador3_anterior = 0;
 int estado_pantalla = 0; //variable que determina la pantalla a usarse
-float angulo = 0; // variable en la que se va a guardar el valor del angulo que va a tener el polaroid
+int angulo = 0; // variable en la que se va a guardar el valor del angulo que va a tener el polaroid
 int inten_max = 0; //Se almacena la maxima intensidad que muestra la fotocelda obtenida del ADC
 
 const int num_lecturas =10; // van a ser la cantidad de lecturas por cada angulo para sacar el promedio, me va a indicar el tamaño del
@@ -46,6 +45,7 @@ const int num_lecturas =10; // van a ser la cantidad de lecturas por cada angulo
 double inten_ins_prom = 0; //variable donde se va a almacenar la intensidd instantanea promedio obtenida de la comparacion de intensidades en cada angulo
 double inten_instant = 0; //variable donde se va a guardar la intensidad promedio instantanea del angulo para verificar si es la maxima y determina con esta el 
                        //angulo de rotación
+float tempC; //variable en la que se almacena la T° en °celcius
 
 String sustancia; //Variable para guardar el nombre de la sustancia seleccionada
 
@@ -99,7 +99,7 @@ void loop()
     estado_pantalla = 1;
     lcd.clear();
   }
-  
+  servo.write (0); 
   if (estado_pantalla == 1)
   {
     //Se va a escribir la pantalla 1 y se va a pasar a pantalla 2
@@ -123,6 +123,8 @@ void loop()
       flag_ok = 0; //Se baja la bandera subir para poder volver a usarla
     }
   }
+
+  
   if (estado_pantalla == 2)
   {
     //Se va a escribir la pantalla 1 y se va a pasar a pantalla 2
@@ -276,10 +278,13 @@ void loop()
     digitalWrite (buzzerPin, HIGH);
     delay (1000);
     digitalWrite (buzzerPin, LOW);
-    Serial.print ("El valor del angulo es: ");
+    temperatura ();
+    Serial.print ("El valor del angulo es: "); 
     Serial.println (angulo);
     Serial.print (" y corresponde a un valor de intensidad de: ");
     Serial.println (inten_max);
+    Serial.print ("La temperatura sensada es de: ");
+    Serial.println (tempC);
     
     /*SACAR DELAY Y PONER FUNCIÓN SENSADO QUE HACE LO SIGUIENTE
      * Enciendo el LED
@@ -397,7 +402,9 @@ void loop()
     lcd.setCursor(1,1);
     lcd.print (char(223));
     lcd.setCursor (2,1);
-    lcd.print (":  ,  ");
+    lcd.print (":");
+    lcd.setCursor (3,1);
+    lcd.print (tempC);
     lcd.setCursor(8,1);
     lcd.print(char(223));
     lcd.setCursor(9,1);
@@ -452,7 +459,8 @@ int sensado () //no se si tiene que ser void o int para mi tiene que ser int por
   //Lo que hace es recorrer todos los angulos de 0° a 180° y comparar los valores de intensidad 
   // obtenidos para determinar el valor de la intensidad mas grande. En cada comparación el 
   // cuando da mas grande la intensidad se guarda el valor de esa intensidad y valor del angulo
-
+  angulo = 0;
+  
   servo.write(0); //inicio el servo en 0  
 
   for (int i= 0; i<=180; i++) //este ciclo me sirve para barrer todos los grados del servo
@@ -527,6 +535,65 @@ int filtro_dato (){
  // Serial.println (inten_ins_prom);
   
   return (inten_ins_prom);    
+}
+
+float temperatura ()
+{
+  float temp_muestra[num_lecturas];
+  int vals_correctos = 0; 
+  int index = 0;
+  int limite = 1; // es el +-1 que tengo en cuenta para la dispersión de los valores medidos al ser T° 
+                  //se va a hacer mas precisa la medicion por eso es 1
+  int limite2 = 6; //cantidad de comparaciones correctas para que sume
+  float total = 0; //para la suma de los promedios de intensidades de muestras correctas
+  float temp = 0;
+  float tempMV = 0;
+  float milivoltios = 0;
+  tempC = 0; 
+
+  for (int i = 0; i<=9; i++) //para que sean 10 datos i final debe ser 9 porque de 0 -9 hay de datos
+  {
+    temp = analogRead (A1); //se lee el valor que da el sensor de T
+    
+    milivoltios = (temp/1024.0)*5000; //transformo los datos binarios a mV
+    tempMV = milivoltios/10; //transformo los mV a °C
+    
+    temp_muestra [i] = tempMV;
+
+  //  Serial.print (temp_muestra [i]);
+  //  Serial.print (" ; ");
+    
+  }
+//  Serial.println ("fin 10 datos");
+
+  for (int i=0; i<=10; i++)//determino el valor que voy a comparar
+  {
+    for (int j=0; j<=10; j++) //voy a ir comparando con todos los valores
+    {
+      if (i != j) //comparo si i es distinto de j
+      {
+        if (abs (temp_muestra [i] - temp_muestra [j]) < limite) //determino si los valores estan a +- 5 datos entre si 
+        {
+          vals_correctos ++; //aumento en 1 cuando los valores con los que compara i estan a +-1 datos
+        }
+      }
+    }
+    
+    if (vals_correctos >= limite2) //si la cantidad de comparaciones similares son += a 6 entonces suma para hacer el promedio
+    {
+      total += temp_muestra [i]; //sumo los valores correctos 
+      index ++; // calculo la cantidad de valores correctos en este angulo
+   }
+    vals_correctos = 0;
+
+  }
+
+  tempC = total / index;
+  //Serial.print ("La T° promedio es: ");
+  //Serial.print (tempC);
+  //Serial.print (" °C"); 
+  
+  return (tempC);
 }
 
 ISR (TIMER2_OVF_vect)
